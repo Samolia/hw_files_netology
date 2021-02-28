2baf3a42db8dcd3d26878446a351d26f76f35aac
samolia <samolia9@gmail.com>
2021-02-28 12:44:52 +0300

    Added a new solution

--- merging_files/unifier.py
+++ merging_files/unifier.py
@@ -1,6 +1,7 @@
 import operator
 import os
 
+# Вариант решения с использованием словаря
 def get_info(file_names):
     '''
     Функция читает файлы/файл и записывает информацию в словарь
@@ -34,3 +35,35 @@ def writing_to_file(my_data, my_file):
     return file_path
 
 print(writing_to_file(get_info(['1.txt', '2.txt', '3.txt']), 'result.txt'))
+
+
+# Вариант решения с использованием списка
+# def get_info(file_names):
+#     '''
+#     Функция читает файлы/файл и записывает информацию в список
+#     :param file_names: Имя файла/файлов для чтения
+#     :return: Список с информацией
+#     '''
+#     my_data = []
+#     for file in file_names:
+#         with open(file, encoding='utf-8') as f:
+#             lines = f.read().splitlines()
+#             my_data.append([file, len(lines)])
+#             my_data[len(my_data)-1] += lines
+#     my_data.sort(key=len)
+#     return my_data
+#
+# def writing_to_file(my_data, my_file):
+#     '''
+#     Функция структурированно записывает в файл информацию (если файла с таким именем нет, то создает его)
+#     :param my_data: Список с информацией
+#     :return: Абсолютный путь к записанному файлу
+#     '''
+#     with open('result.txt', 'w', encoding='utf-8') as f:
+#         for file in my_data:
+#             for elem in file:
+#                 f.write(f'{elem}\n')
+#     file_path = os.path.join(os.getcwd(), my_file)
+#     return file_path
+#
+# print(writing_to_file(get_info(['1.txt', '2.txt', '3.txt']), 'result.txt'))
